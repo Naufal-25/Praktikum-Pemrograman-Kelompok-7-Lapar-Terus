04b33e9e8c7b4efc7e2cf41f73f88c6a30aeb5e1
mizukimura <randomnpc260@gmail.com>
2025-11-08 11:40:14 +0700

    Add function to remove food item by ID

--- fitur/makanan/hapus_makan.py
+++ fitur/makanan/hapus_makan.py
@@ -1,1 +1,11 @@
+def hapus_makanan(daftar_makanan):
+    print("\n=== Hapus Makanan ===")
+    id_makanan = input("Masukkan ID makanan yang ingin dihapus: ")
+
+    for makanan in daftar_makanan:
+        if makanan.id_makanan == id_makanan:
+            daftar_makanan.remove(makanan)
+            print(f"{makanan.nama} berhasil dihapus.\n")
+            return
+    print("Makanan tidak ditemukan.\n")
 #[[Placehoder]]
