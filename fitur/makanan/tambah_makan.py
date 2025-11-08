d1ed1ea182ce81095d5a8f6f50601ea788fbcd27
mizukimura <randomnpc260@gmail.com>
2025-11-08 11:38:46 +0700

    Add function to add new food items

--- fitur/makanan/tambah_makan.py
+++ fitur/makanan/tambah_makan.py
@@ -1,1 +1,13 @@
+from object.makanan import Makanan
+
+def tambah_makanan(daftar_makanan):
+    print("\n=== Tambah Makanan ===")
+    id_makanan = input("ID makanan: ")
+    nama = input("Nama makanan: ")
+    harga = float(input("Harga: "))
+    stok = int(input("Stok: "))
+
+    makanan_baru = Makanan(id_makanan, nama, harga, stok)
+    daftar_makanan.append(makanan_baru)
+    print(f"{nama} berhasil ditambahkan!\n")
 #[[Placehoder]]
