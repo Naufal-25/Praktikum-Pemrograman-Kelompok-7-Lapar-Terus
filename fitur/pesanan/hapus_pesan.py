0815d1caa277d88aef3b1c26fdb14e1936f947fc
mizukimura <randomnpc260@gmail.com>
2025-11-08 11:42:29 +0700

    Add function to delete transactions by ID

--- fitur/pesanan/hapus_pesan.py
+++ fitur/pesanan/hapus_pesan.py
@@ -1,1 +1,11 @@
+def hapus_pesanan(daftar_transaksi):
+    print("\n=== Hapus Transaksi ===")
+    id_transaksi = input("Masukkan ID transaksi: ")
+
+    for transaksi in daftar_transaksi:
+        if transaksi.id_transaksi == id_transaksi:
+            daftar_transaksi.remove(transaksi)
+            print(f"Transaksi {id_transaksi} berhasil dihapus.\n")
+            return
+    print("Transaksi tidak ditemukan.\n")
 #[[Placehoder]]
