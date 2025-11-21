d042629527b8310909954122b4616645fe0e899e
Naufal-25 <naufal_2005@student.uns.ac.id>
2025-11-21 20:46:59 +0700

    Update makanan.py

--- object/makanan.py
+++ object/makanan.py
@@ -7,3 +7,11 @@ class Makanan:
     
     def __str__(self):
         return f"{self.nama} - Rp{self.harga} | Stok: {self.stok}"
+
+    def to_text(self):
+        return f"{self.id_makanan},{self.nama},{self.harga},{self.stock}"
+
+    @staticmethod
+    def from_text(line):
+        id_makanan, nama, harga, stok = line.strip().split(",")
+        return Makanan(id_makanan, nama, float(harga), int(stok))
