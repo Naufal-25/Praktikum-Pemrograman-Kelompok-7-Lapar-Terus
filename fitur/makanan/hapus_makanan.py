620cc7a9dad9e7dc2984cdd26a5ed6cf5bdc9f58
Naufal-25 <naufal_2005@student.uns.ac.id>
2025-11-23 20:08:09 +0700

    fix a bit

--- fitur/makanan/hapus_makanan.py
+++ fitur/makanan/hapus_makanan.py
@@ -3,10 +3,13 @@ from fitur.makanan.datamanage_makanan import save_makanan
 def hapus_makanan(daftar_makanan):
     print("\n=== Hapus Makanan ===")
     id_makanan = input("Masukkan ID makanan yang ingin dihapus: ")
+    if not id_makanan:
+        print("ID tidak boleh kosong")
+        return
 
-    for makanan in daftar_makanan:
+    for i, makanan in enumerate(daftar_makanan):
         if makanan.id_makanan == id_makanan:
-            daftar_makanan.remove(makanan)
+            daftar_makanan.pop(i)
 
             save_makanan(daftar_makanan)
 
